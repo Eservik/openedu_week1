cfda6d187300a72bd3cb0a797813ac4e1a3c3644
Eservik <73242477+Eservik@users.noreply.github.com>
2021-07-18 13:08:19 +0300

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+const http = require('http');
+
+const server = http.createServer();
+
+server.on('request', (req,res) => {
+  res.end('Проверка');
+});
+
+server.listen (3000, () => console.log('Сервер работает'));
